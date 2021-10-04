c6e19f2f6bfc37176d5a886b5ba9c78bfe8eb4e2
Kimkyeongbeom4844 <bibix321@naver.com>
2021-10-04 19:22:04 +0900

    버스노선 자바스크립트 파일생성

---
+++ scripts/bus.js
@@ -0,0 +1,72 @@
+    markerPosition1 = new kakao.maps.LatLng(33.45755993777187, 126.56107582852171); //약학대학
+    var busmarker1 = new kakao.maps.Marker({
+        position: markerPosition1,
+        image: markerImage
+    });
+    busmarker1.setMap(map);
+    markerPosition2 = new kakao.maps.LatLng(33.454610128657876, 126.56063087550932); //학생회관
+    var busmarker2 = new kakao.maps.Marker({
+        position: markerPosition2,
+        image: markerImage
+    });
+    busmarker2.setMap(map);
+    markerPosition3 = new kakao.maps.LatLng(33.45323099738815, 126.5575509319587); //인문대학(서)
+    var busmarker3 = new kakao.maps.Marker({
+        position: markerPosition3,
+        image: markerImage
+    });
+    busmarker3.setMap(map);
+    markerPosition4 = new kakao.maps.LatLng(33.45149156127691, 126.55836642987437); //학생생활관
+    var busmarker4 = new kakao.maps.Marker({
+        position: markerPosition4,
+        image: markerImage
+    });
+    busmarker4.setMap(map);
+    markerPosition5 = new kakao.maps.LatLng(33.45305459367828, 126.5592834991402); //인문대학(동)
+    var busmarker5 = new kakao.maps.Marker({
+        position: markerPosition5,
+        image: markerImage
+    });
+    busmarker5.setMap(map);
+    markerPosition6 = new kakao.maps.LatLng(33.45311704756447, 126.56100410021769); //중앙도서관
+    var busmarker6 = new kakao.maps.Marker({
+        position: markerPosition6,
+        image: markerImage
+    });
+    busmarker6.setMap(map);
+    markerPosition7 = new kakao.maps.LatLng(33.453057621098026, 126.56396491302868); //의학전문대학원
+    var busmarker7 = new kakao.maps.Marker({
+        position: markerPosition7,
+        image: markerImage
+    });
+    busmarker7.setMap(map);
+    markerPosition8 = new kakao.maps.LatLng(33.45474511612683, 126.56439208250133); //공과대학 4호관
+    var busmarker8 = new kakao.maps.Marker({
+        position: markerPosition8,
+        image: markerImage
+    });
+    busmarker8.setMap(map);
+    markerPosition9 = new kakao.maps.LatLng(33.45523826971047, 126.56618586795527); //해양과학대학 4호관
+    var busmarker9 = new kakao.maps.Marker({
+        position: markerPosition9,
+        image: markerImage
+    });
+    busmarker9.setMap(map);
+    markerPosition10 = new kakao.maps.LatLng(33.45562907037793, 126.56386061632756); //교양강의동
+    var busmarker10 = new kakao.maps.Marker({
+        position: markerPosition10,
+        image: markerImage
+    });
+    busmarker10.setMap(map);
+    markerPosition11 = new kakao.maps.LatLng(33.45627626917144, 126.56266613546954); //본관
+    var busmarker11 = new kakao.maps.Marker({
+        position: markerPosition11,
+        image: markerImage
+    });
+    busmarker11.setMap(map);
+    markerPosition12 = new kakao.maps.LatLng(33.45731481654195, 126.56315033095102); //해양과학대학 1호관
+    var busmarker12 = new kakao.maps.Marker({
+        position: markerPosition12,
+        image: markerImage
+    });
+    busmarker12.setMap(map);
