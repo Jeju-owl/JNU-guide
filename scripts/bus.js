a2b0926163854f2e23936fe24dd15a7c12cf1071
Kimkyeongbeom4844 <bibix321@naver.com>
2021-10-06 23:30:43 +0900

    커스텀 오버레이 try2

--- scripts/bus.js
+++ scripts/bus.js
@@ -13,43 +13,6 @@
     });
     // 교내순환버스마커가 지도 위에 표시되도록 설정합니다
     busmarker.setMap(map);
-
-    var content = '<div class="wrap">' + 
-            '    <div class="info">' + 
-            '        <div class="title">' + 
-            '            카카오 스페이스닷원' + 
-            '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' + 
-            '        </div>' + 
-            '        <div class="body">' + 
-            '            <div class="img">' +
-            '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
-            '           </div>' + 
-            '            <div class="desc">' + 
-            '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' + 
-            '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' + 
-            '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' + 
-            '            </div>' + 
-            '        </div>' + 
-            '    </div>' +    
-            '</div>';
-
-    // 마커 위에 커스텀오버레이를 표시합니다
-    // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
-    var overlay = new kakao.maps.CustomOverlay({
-        content: content,
-        map: map,
-        position: busmarker.getPosition()       
-    });
-
-    // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
-    kakao.maps.event.addListener(busmarker, 'click', function() {
-        overlay.setMap(map);
-    });
-
-    // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
-    function closeOverlay() {
-        overlay.setMap(null);     
-    }
     
     markerPosition1 = new kakao.maps.LatLng(33.45755993777187, 126.56107582852171); //약학대학
     var busmarker1 = new kakao.maps.Marker({
