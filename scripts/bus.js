bfbbec2fbca89716572db1115466b16cf1b520eb
Kimkyeongbeom4844 <bibix321@naver.com>
2021-10-09 21:25:42 +0900

    웹앱 개선
    지인들의 의견을 수렴하여 웹앱 개선사항 반영

--- scripts/bus.js
+++ scripts/bus.js
@@ -1,8 +1,8 @@
     //---------------------버스마커부분------------------------------
     
     var imageSrc = 'https://github.com/Jeju-owl/JNU-guide/blob/feature-3/images/busimage.png?raw=true', // 마커이미지의 주소입니다, remove.bg사이트에서 png 바깥배경 없앳음 
-    imageSize = new kakao.maps.Size(25, 20), // 교내순환버스마커 이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(10, 10)}; // 교내순환버스마커 이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    imageSize = new kakao.maps.Size(28, 20), // 교내순환버스마커 이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(11, 10)}; // 교내순환버스마커 이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption), // 교내순환버스마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
         markerPosition = new kakao.maps.LatLng(33.459811596777094, 126.5616883626409); // 마커가 표시될 위치입니다(정문)
