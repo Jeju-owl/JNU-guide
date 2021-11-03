10725b4f94e058f89e22a739cbe9c661e8594b60
Kimkyeongbeom4844 <bibix321@naver.com>
2021-11-03 13:51:24 +0900

    맵레벨별 오버레이 실험

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -3126,3 +3126,56 @@ var content12 ='<div class="wrap">'+
         ATMoverlay6.setMap(null);
         ATMoverlay7.setMap(null);
     }
+    // 지도가 확대 또는 축소되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(map, 'zoom_changed', function() {        
+        
+        // 지도의 현재 레벨을 얻어옵니다
+        var level = map.getLevel();
+        
+        if (level >= 5) {
+            overlay.setMap(null);
+            overlay1.setMap(null);
+            overlay2.setMap(null);
+            overlay3.setMap(null);
+            overlay4.setMap(null);
+            overlay5.setMap(null);
+            overlay6.setMap(null);
+            overlay7.setMap(null);
+            overlay8.setMap(null);
+            overlay9.setMap(null);
+            overlay10.setMap(null);
+            overlay11.setMap(null);
+            overlay12.setMap(null);
+            ATMoverlay.setMap(null);
+            ATMoverlay1.setMap(null);
+            ATMoverlay2.setMap(null);
+            ATMoverlay3.setMap(null);
+            ATMoverlay4.setMap(null);
+            ATMoverlay5.setMap(null);
+            ATMoverlay6.setMap(null);
+            ATMoverlay7.setMap(null);
+        }
+        else {
+            overlay.setMap(map);
+            overlay1.setMap(map);
+            overlay2.setMap(map);
+            overlay3.setMap(map);
+            overlay4.setMap(map);
+            overlay5.setMap(map);
+            overlay6.setMap(map);
+            overlay7.setMap(map);
+            overlay8.setMap(map);
+            overlay9.setMap(map);
+            overlay10.setMap(map);
+            overlay11.setMap(map);
+            overlay12.setMap(map);
+            ATMoverlay.setMap(map);
+            ATMoverlay1.setMap(map);
+            ATMoverlay2.setMap(map);
+            ATMoverlay3.setMap(map);
+            ATMoverlay4.setMap(map);
+            ATMoverlay5.setMap(map);
+            ATMoverlay6.setMap(map);
+            ATMoverlay7.setMap(map);
+        }     
+    });
