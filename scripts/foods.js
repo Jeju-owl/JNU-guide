05898e8b011ec17aa9742fc7fdf03645d1c6ace7
Kimkyeongbeom4844 <bibix321@naver.com>
2021-11-09 22:51:43 +0900

    한식 배치 완료
    주석으로 진짜 보기 좋게 해놓음

--- scripts/foods.js
+++ scripts/foods.js
@@ -92,36 +92,32 @@ var pizzamarkerImage = new kakao.maps.MarkerImage(pizzaimageSrc, pizzaimageSize,
 var beermarkerImage = new kakao.maps.MarkerImage(beerimageSrc, beerimageSize, beerimageOption);
 
 // 식당 위치 정보
-var koreanmarkerPosition = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 콘킹부대찌개위치
-var koreanmarkerPosition1 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 개미와 베짱이
-var koreanmarkerPosition2 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 김밥천국
-var koreanmarkerPosition3 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 맵수다
-var koreanmarkerPosition4 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 한솥도시락
-var koreanmarkerPosition5 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 밥먹젠
-var koreanmarkerPosition6 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 불노리왕막창
-var koreanmarkerPosition7 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 산들애국수
-var koreanmarkerPosition8 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 산아름국수
-var koreanmarkerPosition9 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 삐삐네 정식
-var koreanmarkerPosition10 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 상아탑
-var koreanmarkerPosition11 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 와랑와랑
-var koreanmarkerPosition12 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 통큰이층
-var koreanmarkerPosition13 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 학교종이 땡땡땡
-var koreanmarkerPosition14 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 흥부네
-var koreanmarkerPosition15 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 배꼽시계
-var koreanmarkerPosition16 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 매력식당
-var koreanmarkerPosition17 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 봉구스밥버거
-var koreanmarkerPosition18 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 신전떡볶이
-var koreanmarkerPosition19 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 신전떡볶이
-var koreanmarkerPosition20 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 글로벌하우스
-var koreanmarkerPosition21 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 옛날아우네 순대
-var koreanmarkerPosition22 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 이삭토스트
-var koreanmarkerPosition23 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 밀면한라마루
-var koreanmarkerPosition24 = new kakao.maps.LatLng(33.460629678377366, 126.5610173329148); // 엄마네
+var koreanmarkerPosition = new kakao.maps.LatLng(33.45916954797216,126.5579752302018); // 이삭토스트-토스트
+var koreanmarkerPosition1 = new kakao.maps.LatLng(33.460632008508654,126.56103883449029); // 콘킹부대찌개-한식
+var koreanmarkerPosition2 = new kakao.maps.LatLng(33.46022359435553,126.5609145026397); // 김밥천구-김밥
+var koreanmarkerPosition3 = new kakao.maps.LatLng(33.460623097403385,126.56106846021184); // 맵수다-갈비
+var koreanmarkerPosition4 = new kakao.maps.LatLng(33.46033618927183,126.5615216842466); // 한솥도시락-덫밥
+var koreanmarkerPosition5 = new kakao.maps.LatLng(33.460376608358665,126.56147845425443); // 밥먹젠-한식
+var koreanmarkerPosition6 = new kakao.maps.LatLng(33.46065412115623,126.56155504285272); // 산들애국수-국수
+var koreanmarkerPosition7 = new kakao.maps.LatLng(33.46067993195596,126.56184265374061); // 상아탑식당-갈비
+var koreanmarkerPosition8 = new kakao.maps.LatLng(33.460383777268326,126.5622313822297); // 학교종이 땡땡땡-갈비
+var koreanmarkerPosition9 = new kakao.maps.LatLng(33.460651648449094,126.56149320449096); // 봉구스밥버거-덫밥
+var koreanmarkerPosition10 = new kakao.maps.LatLng(33.460661586622464,126.5617540035518); // 신전떡볶이-떡
+var koreanmarkerPosition11 = new kakao.maps.LatLng(33.46037934531052,126.56225291776347); // 황칠해장국-한식
+var koreanmarkerPosition12 = new kakao.maps.LatLng(33.44991589041655,126.55769951894841); // 매력식당-덫밥
+var koreanmarkerPosition13 = new kakao.maps.LatLng(33.45019544707126,126.5583487997336); // 통큰이층-덫밥
+var koreanmarkerPosition14 = new kakao.maps.LatLng(33.450022217578294,126.5584410982346); // 산아름국수-국수
+var koreanmarkerPosition15 = new kakao.maps.LatLng(33.44932447888236,126.55808971038034); // 불노리왕막창-갈비
+var koreanmarkerPosition16 = new kakao.maps.LatLng(33.44911220396614,126.55924428598729); // 개미와 베짱이-한식
+var koreanmarkerPosition17 = new kakao.maps.LatLng(33.4489502053445,126.55932576952746); // 흥부네-덫밥
+var koreanmarkerPosition18 = new kakao.maps.LatLng(33.44929790080385,126.55822159708907); // 옛날아우네 순대-한식
+var koreanmarkerPosition19 = new kakao.maps.LatLng(33.44898577786935,126.55855390255299); // 엄마네 정식-한식
+var koreanmarkerPosition20 = new kakao.maps.LatLng(33.456795261451575,126.55964373072615); // 글로벌하우스-한식
 
 // 마커를 생성합니다 ------이부분부터 순서신경 잘해야됨------
 var koreanmarker = new kakao.maps.Marker({
     position: koreanmarkerPosition, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: toastmarkerImage // 마커이미지 설정 
 });
 var koreanmarker1 = new kakao.maps.Marker({
     position: koreanmarkerPosition1, 
@@ -129,15 +125,15 @@ var koreanmarker1 = new kakao.maps.Marker({
 });
 var koreanmarker2 = new kakao.maps.Marker({
     position: koreanmarkerPosition2, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: gimbapmarkerImage // 마커이미지 설정 
 });
 var koreanmarker3 = new kakao.maps.Marker({
     position: koreanmarkerPosition3, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: galbimarkerImage // 마커이미지 설정 
 });
 var koreanmarker4 = new kakao.maps.Marker({
     position: koreanmarkerPosition4, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: ricemarkerImage // 마커이미지 설정 
 });
 var koreanmarker5 = new kakao.maps.Marker({
     position: koreanmarkerPosition5, 
@@ -145,23 +141,23 @@ var koreanmarker5 = new kakao.maps.Marker({
 });
 var koreanmarker6 = new kakao.maps.Marker({
     position: koreanmarkerPosition6, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: noodlemarkerImage // 마커이미지 설정 
 });
 var koreanmarker7 = new kakao.maps.Marker({
     position: koreanmarkerPosition7, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: galbimarkerImage // 마커이미지 설정 
 });
 var koreanmarker8 = new kakao.maps.Marker({
     position: koreanmarkerPosition8, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: galbimarkerImage // 마커이미지 설정 
 });
 var koreanmarker9 = new kakao.maps.Marker({
     position: koreanmarkerPosition9, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: ricemarkerImage // 마커이미지 설정 
 });
 var koreanmarker10 = new kakao.maps.Marker({
     position: koreanmarkerPosition10, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: ddeokmarkerImage // 마커이미지 설정 
 });
 var koreanmarker11 = new kakao.maps.Marker({
     position: koreanmarkerPosition11, 
@@ -169,19 +165,19 @@ var koreanmarker11 = new kakao.maps.Marker({
 });
 var koreanmarker12 = new kakao.maps.Marker({
     position: koreanmarkerPosition12, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: ricemarkerImage // 마커이미지 설정 
 });
 var koreanmarker13 = new kakao.maps.Marker({
     position: koreanmarkerPosition13, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: ricemarkerImage // 마커이미지 설정 
 });
 var koreanmarker14 = new kakao.maps.Marker({
     position: koreanmarkerPosition14, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: noodlemarkerImage // 마커이미지 설정 
 });
 var koreanmarker15 = new kakao.maps.Marker({
     position: koreanmarkerPosition15, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: galbimarkerImage // 마커이미지 설정 
 });
 var koreanmarker16 = new kakao.maps.Marker({
     position: koreanmarkerPosition16, 
@@ -189,7 +185,7 @@ var koreanmarker16 = new kakao.maps.Marker({
 });
 var koreanmarker17 = new kakao.maps.Marker({
     position: koreanmarkerPosition17, 
-    image: koreanmarkerImage // 마커이미지 설정 
+    image: ricemarkerImage // 마커이미지 설정 
 });
 var koreanmarker18 = new kakao.maps.Marker({
     position: koreanmarkerPosition18, 
@@ -203,22 +199,6 @@ var koreanmarker20 = new kakao.maps.Marker({
     position: koreanmarkerPosition20, 
     image: koreanmarkerImage // 마커이미지 설정 
 });
-var koreanmarker21 = new kakao.maps.Marker({
-    position: koreanmarkerPosition21, 
-    image: koreanmarkerImage // 마커이미지 설정 
-});
-var koreanmarker22 = new kakao.maps.Marker({
-    position: koreanmarkerPosition22, 
-    image: koreanmarkerImage // 마커이미지 설정 
-});
-var koreanmarker23 = new kakao.maps.Marker({
-    position: koreanmarkerPosition23, 
-    image: koreanmarkerImage // 마커이미지 설정 
-});
-var koreanmarker24 = new kakao.maps.Marker({
-    position: koreanmarkerPosition24, 
-    image: koreanmarkerImage // 마커이미지 설정 
-});
 
 
 // 지도의 확대 축소 레벨이 바뀔때마다 실행되는 이벤트리스터입니다.
@@ -226,8 +206,48 @@ kakao.maps.event.addListener(map, 'zoom_changed', function() {
 var level = map.getLevel();// 지도의 현재 레벨을 얻어옵니다
 if (level >= 3) {
     koreanmarker.setMap(null);
+    koreanmarker1.setMap(null);
+    koreanmarker2.setMap(null);
+    koreanmarker3.setMap(null);
+    koreanmarker4.setMap(null);
+    koreanmarker5.setMap(null);
+    koreanmarker6.setMap(null);
+    koreanmarker7.setMap(null);
+    koreanmarker8.setMap(null);
+    koreanmarker9.setMap(null);
+    koreanmarker10.setMap(null);
+    koreanmarker11.setMap(null);
+    koreanmarker12.setMap(null);
+    koreanmarker13.setMap(null);
+    koreanmarker14.setMap(null);
+    koreanmarker15.setMap(null);
+    koreanmarker16.setMap(null);
+    koreanmarker17.setMap(null);
+    koreanmarker18.setMap(null);
+    koreanmarker19.setMap(null);
+    koreanmarker20.setMap(null);
 }
 else {
     koreanmarker.setMap(map);
+    koreanmarker1.setMap(map);
+    koreanmarker2.setMap(map);
+    koreanmarker3.setMap(map);
+    koreanmarker4.setMap(map);
+    koreanmarker5.setMap(map);
+    koreanmarker6.setMap(map);
+    koreanmarker7.setMap(map);
+    koreanmarker8.setMap(map);
+    koreanmarker9.setMap(map);
+    koreanmarker10.setMap(map);
+    koreanmarker11.setMap(map);
+    koreanmarker12.setMap(map);
+    koreanmarker13.setMap(map);
+    koreanmarker14.setMap(map);
+    koreanmarker15.setMap(map);
+    koreanmarker16.setMap(map);
+    koreanmarker17.setMap(map);
+    koreanmarker18.setMap(map);
+    koreanmarker19.setMap(map);
+    koreanmarker20.setMap(map);
 }     
 });
