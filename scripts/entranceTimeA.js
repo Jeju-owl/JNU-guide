f5659d20d49b41be69e2b50bc80c752360faad22
KimSeungDeok <tmdejr1117@gmail.com>
2021-11-15 14:52:06 +0900

    순환버스 남은 시간 수정

--- scripts/entranceTimeA.js
+++ scripts/entranceTimeA.js
@@ -13,29 +13,29 @@ function entranceTimeA() {
     }
 
     if(nowHour<19){
-        if(nowHour<=18){
+        if(nowHour==18){
             if(nowMinute>40){
                 document.querySelector("#Atime").innerHTML = "운행 없음";
             }
-            else if(nowMinute<=40){
+            else if(nowMinute<=40 && nowMinute>20){
                 document.querySelector("#Atime").innerHTML = Number(40-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=20){
                 document.querySelector("#Atime").innerHTML = Number(20-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=17){
+        else if(nowHour==17){
             if(nowMinute>40){
                 document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
             }
-            else if(nowMinute<=40){
+            else if(nowMinute<=40 && nowMinute>20){
                 document.querySelector("#Atime").innerHTML = Number(40-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=20){
                 document.querySelector("#Atime").innerHTML = Number(20-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=16){
+        else if(nowHour==16){
             if(nowMinute>30){ 
                 document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
             }
@@ -43,97 +43,97 @@ function entranceTimeA() {
                 document.querySelector("#Atime").innerHTML = Number(30-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=15){
+        else if(nowHour==15){
             if(nowMinute>45){ 
-                document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#Atime").innerHTML = Number(90-nowMinute)+":"+Number(59-nowSecond);
             }
-            else if(nowMinute<=45){
+            else if(nowMinute<=45 && nowMinute>25){
                 document.querySelector("#Atime").innerHTML = Number(45-nowMinute)+":"+Number(59-nowSecond);
             }
-            else if(nowMinute<=25){
+            else if(nowMinute<=25 && nowMinute>5){
                 document.querySelector("#Atime").innerHTML = Number(25-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=5){
                 document.querySelector("#Atime").innerHTML = Number(5-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=14){
+        else if(nowHour==14){
             if(nowMinute>30){ 
-                document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#Atime").innerHTML = Number(65-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=30){
                 document.querySelector("#Atime").innerHTML = Number(30-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=13){
+        else if(nowHour==13){
             if(nowMinute>45){ 
-                document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#Atime").innerHTML = Number(90-nowMinute)+":"+Number(59-nowSecond);
             }
-            else if(nowMinute<=45){
+            else if(nowMinute<=45 && nowMinute>25){
                 document.querySelector("#Atime").innerHTML = Number(45-nowMinute)+":"+Number(59-nowSecond);
             }
-            else if(nowMinute<=25){
+            else if(nowMinute<=25 && nowMinute>5){
                 document.querySelector("#Atime").innerHTML = Number(25-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=5){
                 document.querySelector("#Atime").innerHTML = Number(5-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=12){
+        else if(nowHour==12){
             if(nowMinute>40){ 
-                document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#Atime").innerHTML = Number(65-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=40){
                 document.querySelector("#Atime").innerHTML = Number(40-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=11){
+        else if(nowHour==11){
             if(nowMinute>20){ 
-                document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#Atime").innerHTML = "1:" + Number(40-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=20){
                 document.querySelector("#Atime").innerHTML = Number(20-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=10){
+        else if(nowHour==10){
             if(nowMinute>45){ 
-                document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#Atime").innerHTML = Number(80-nowMinute)+":"+Number(59-nowSecond);
             }
-            else if(nowMinute<=45){
+            else if(nowMinute<=45 && nowMinute>25){
                 document.querySelector("#Atime").innerHTML = Number(45-nowMinute)+":"+Number(59-nowSecond);
             }
-            else if(nowMinute<=25){
+            else if(nowMinute<=25 && nowMinute>5){
                 document.querySelector("#Atime").innerHTML = Number(25-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=5){
                 document.querySelector("#Atime").innerHTML = Number(5-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=9){
+        else if(nowHour==9){
             if(nowMinute>30){ 
-                document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#Atime").innerHTML = Number(65-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=30){
                 document.querySelector("#Atime").innerHTML = Number(30-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=8){
+        else if(nowHour==8){
             if(nowMinute>45){ 
-                document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#Atime").innerHTML = Number(90-nowMinute)+":"+Number(59-nowSecond);
             }
-            else if(nowMinute<=45){
+            else if(nowMinute<=45 && nowMinute>25){
                 document.querySelector("#Atime").innerHTML = Number(45-nowMinute)+":"+Number(59-nowSecond);
             }
-            else if(nowMinute<=25){
+            else if(nowMinute<=25 && nowMinute>5){
                 document.querySelector("#Atime").innerHTML = Number(25-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=5){
                 document.querySelector("#Atime").innerHTML = Number(5-nowMinute)+":"+Number(59-nowSecond);
             }
         }
-        else if(nowHour<=7){
+        else if(nowHour==7){
             if(nowMinute>30){ 
-                document.querySelector("#Atime").innerHTML = Number(60-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#Atime").innerHTML = Number(65-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowHour<=30){
                 document.querySelector("#Atime").innerHTML = "운행 없음";
