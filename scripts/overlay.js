4e793e754e66a59120a3720b6a0509ebbfccfb0a
KimSeungDeok <tmdejr1117@gmail.com>
2021-11-15 18:16:17 +0900

    시간 테스트

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -200,7 +200,7 @@
             '<div id="busbody">'+
                 '<div id="businfo" class="clearfix">'+
                     '<div id="businfoleft">'+
-                        '<strong>A</strong><br>(<a>반시계</a> 방향)<br><p id="Atime">로딩 중</p>'+
+                        '<strong>A</strong><br>(<a>반시계</a> 방향)<br><p id="AtimePharm">로딩 중</p>'+
                     '</div>'+
                     '<div id="businforight">'+
                         '<strong>B</strong><br>(<a>시계</a> 방향)<br><p id="Btime">로딩 중</p>'+
@@ -420,7 +420,7 @@
             '<div id="busbody">'+
                 '<div id="businfo" class="clearfix">'+
                     '<div id="businfoleft">'+
-                        '<strong>A</strong><br>(<a>반시계</a> 방향)<br><p id="Atime">로딩 중</p>'+
+                        '<strong>A</strong><br>(<a>반시계</a> 방향)<br><p id="AtimeMarine">로딩 중</p>'+
                     '</div>'+
                     '<div id="businforight">'+
                         '<strong>B</strong><br>(<a>시계</a> 방향)<br><p id="Btime">로딩 중</p>'+
--- scripts/pharmacyTimeA.js
+++ scripts/pharmacyTimeA.js
@@ -9,145 +9,145 @@ function pharmacyTimeA() {
 
 
     if(nowHour>=19 || now.getDay()==0 || now.getDay()==6 || nowHour<=6){
-        document.querySelector("#Atime").innerHTML = "운행 없음";
+        document.querySelector("#AtimePharm").innerHTML = "운행 없음";
     }
 
     if(nowHour<19){
         if(nowHour==18){
             if(nowMinute>41){
-                document.querySelector("#Atime").innerHTML = "운행 없음";
+                document.querySelector("#AtimePharm").innerHTML = "운행 없음";
             }
             else if(nowMinute<=41 && nowMinute>21){
-                document.querySelector("#Atime").innerHTML = Number(41-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(41-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=21 && nowMinute>1){
-                document.querySelector("#Atime").innerHTML = Number(21-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(21-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=1){
-                document.querySelector("#Atime").innerHTML = Number(1-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(1-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==17){
             if(nowMinute>41){
-                document.querySelector("#Atime").innerHTML = Number(61-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(61-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=41 && nowMinute>21){
-                document.querySelector("#Atime").innerHTML = Number(41-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(41-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=21 && nowMinute>1){
-                document.querySelector("#Atime").innerHTML = Number(21-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(21-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=1){
-                document.querySelector("#Atime").innerHTML = Number(1-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(1-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==16){
             if(nowMinute>31){ 
-                document.querySelector("#Atime").innerHTML = Number(61-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(61-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=31){
-                document.querySelector("#Atime").innerHTML = Number(31-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(31-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==15){
             if(nowMinute>46){ 
-                document.querySelector("#Atime").innerHTML = Number(91-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(91-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=46 && nowMinute>26){
-                document.querySelector("#Atime").innerHTML = Number(46-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(46-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=26 && nowMinute>6){
-                document.querySelector("#Atime").innerHTML = Number(26-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(26-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=6){
-                document.querySelector("#Atime").innerHTML = Number(6-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(6-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==14){
             if(nowMinute>31){ 
-                document.querySelector("#Atime").innerHTML = Number(66-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(66-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=31){
-                document.querySelector("#Atime").innerHTML = Number(31-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(31-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==13){
             if(nowMinute>46){ 
-                document.querySelector("#Atime").innerHTML = Number(91-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(91-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=46 && nowMinute>26){
-                document.querySelector("#Atime").innerHTML = Number(46-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(46-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=26 && nowMinute>6){
-                document.querySelector("#Atime").innerHTML = Number(26-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(26-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=6){
-                document.querySelector("#Atime").innerHTML = Number(6-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(6-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==12){
             if(nowMinute>41){ 
-                document.querySelector("#Atime").innerHTML = Number(66-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(66-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=41){
-                document.querySelector("#Atime").innerHTML = Number(41-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(41-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==11){
             if(nowMinute>21){
                 if(nowMinute<=41 && nowMinute>21){
-                    document.querySelector("#Atime").innerHTML = Number(12-nowHour) + ":" + Number(41-nowMinute)+":"+Number(59-nowSecond);
+                    document.querySelector("#AtimePharm").innerHTML = Number(12-nowHour) + ":" + Number(41-nowMinute)+":"+Number(59-nowSecond);
                 }
                 else if(nowMinute>41){
-                    document.querySelector("#Atime").innerHTML = Number(101-nowMinute)+":"+Number(59-nowSecond);
+                    document.querySelector("#AtimePharm").innerHTML = Number(101-nowMinute)+":"+Number(59-nowSecond);
                 }
             }
             else if(nowMinute<=21){
-                document.querySelector("#Atime").innerHTML = Number(21-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(21-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==10){
             if(nowMinute>46){ 
-                document.querySelector("#Atime").innerHTML = Number(81-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(81-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=46 && nowMinute>26){
-                document.querySelector("#Atime").innerHTML = Number(46-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(46-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=26 && nowMinute>6){
-                document.querySelector("#Atime").innerHTML = Number(26-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(26-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=6){
-                document.querySelector("#Atime").innerHTML = Number(6-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(6-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==9){
             if(nowMinute>31){ 
-                document.querySelector("#Atime").innerHTML = Number(66-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(66-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=31){
-                document.querySelector("#Atime").innerHTML = Number(31-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(31-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==8){
             if(nowMinute>46){ 
-                document.querySelector("#Atime").innerHTML = Number(91-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(91-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=46 && nowMinute>26){
-                document.querySelector("#Atime").innerHTML = Number(46-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(46-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=26 && nowMinute>6){
-                document.querySelector("#Atime").innerHTML = Number(26-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(26-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowMinute<=6){
-                document.querySelector("#Atime").innerHTML = Number(6-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(6-nowMinute)+":"+Number(59-nowSecond);
             }
         }
         else if(nowHour==7){
             if(nowMinute>31){ 
-                document.querySelector("#Atime").innerHTML = Number(66-nowMinute)+":"+Number(59-nowSecond);
+                document.querySelector("#AtimePharm").innerHTML = Number(66-nowMinute)+":"+Number(59-nowSecond);
             }
             else if(nowHour<=31){
-                document.querySelector("#Atime").innerHTML = "운행 없음";
+                document.querySelector("#AtimePharm").innerHTML = "운행 없음";
             }
         }
     }
